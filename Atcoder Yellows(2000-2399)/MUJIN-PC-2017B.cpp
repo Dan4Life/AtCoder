ef1c990eb9f710dcc4186ea9c5ac17017d18748c
Daniel <danielilozor2006@gmail.com>
2022-09-23 17:03:50 +0100

    Shortened my code

--- Atcoder Yellows(2000-2399)/MUJIN-PC-2017B.cpp
+++ Atcoder Yellows(2000-2399)/MUJIN-PC-2017B.cpp
@@ -6,23 +6,18 @@ Same as editorial
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define pb push_back
-#define all(a) a.begin(), a.end()
-
 const int maxn = (int)5e2+10; 
-const int INF = (int)1e9; 
 
 int n;
-string a[maxn], b[maxn];
+string a[maxn];
 int cnt[maxn], cnt2[maxn];
 
 int32_t main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0);
     cin >> n; 
-    int ans = 2*n+n, x = 0; bool ok = 0;
-    for(int i = 0; i < n; i++) cin >> a[i], b[i]=a[i];
+    int ans = 2*n+2, x = 0; bool ok = 0;
+    for(int i = 0; i < n; i++) cin >> a[i];
  
     for(int i = 0; i < n; i++){
         int cnt = 0;
@@ -34,15 +29,12 @@ int32_t main()
 
     for(int i = 0; i < n; i++){
         int sum = 0, xx=0;
-        for(int j = 0; j < n; j++) a[j]=b[j];
         for(int j = 0; j < n; j++){
             if(a[i][j]=='#') continue;
             int cnt = 0;
             for(int k = 0; k < n; k++)
                 if(a[k][i]=='#') cnt=1;
-            sum++;
-            if(!cnt) xx=1; 
-            a[i][j]='#';
+            sum++; if(!cnt) xx=1;
         }
         ans = min(ans, sum+xx);
     }
