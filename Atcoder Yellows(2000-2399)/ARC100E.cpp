b7793f1e03a5935e3f5012dcad1b9543c98395dd
Daniel <danielilozor2006@gmail.com>
2022-09-29 15:29:08 +0100

    Minor edit

--- Atcoder Yellows(2000-2399)/ARC100E.cpp
+++ Atcoder Yellows(2000-2399)/ARC100E.cpp
@@ -3,7 +3,7 @@ Solved with help (https://cp-algorithms.com/algebra/all-submasks.html#enumeratin
 The crux of the problem is finding out how to enumerate all submasks quickly. (New technique learnt)
 Turns out it's standard and can be done in O(3^n)
 Also this is different from editorial but way slower in comparison.
-My editorial solution is commented below
+My editorial solution in O(N*2^N) is commented below
 */
 
 #include <bits/stdc++.h>
@@ -43,12 +43,22 @@ int32_t main()
     
     for(int i = 0; i < n; i++){
         for(int m = 0; m < (1<<n); m++){
+
             if((m>>i)&1) continue;
             int x = m|(1<<i);
             for(auto u : S[m]) S[x].insert(u);
             while((int)S[x].size()>2) S[x].erase(S[x].begin());
+
+
+            //Alternatively You can do this instead
+            if((m>>i)&1){
+                int x = m^(1<<i);
+                for(auto u : S[x]) S[m].insert(u);
+                while((int)S[m].size()>2) S[m].erase(S[m].begin());
+            }
         }
     }
+    
     for(int m = 1; m < (1<<n); m++)
         ans = max(ans, *prev(S[m].end())+*prev(prev(S[m].end()))),
         cout << ans << "\n";
