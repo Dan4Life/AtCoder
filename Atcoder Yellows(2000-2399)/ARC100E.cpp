b0c0073ed6765e36029292129aff03763b88612c
Daniel <danielilozor2006@gmail.com>
2022-09-29 15:04:16 +0100

    Added editorial solution

--- Atcoder Yellows(2000-2399)/ARC100E.cpp
+++ Atcoder Yellows(2000-2399)/ARC100E.cpp
@@ -1,7 +1,9 @@
 /*
 Solved with help (https://cp-algorithms.com/algebra/all-submasks.html#enumerating-all-submasks-of-a-given-mask)
-The crux of the problem is finding out how to enumerate all submasks quickly. 
+The crux of the problem is finding out how to enumerate all submasks quickly. (New technique learnt)
 Turns out it's standard and can be done in O(3^n)
+Also this is different from editorial but way slower in comparison.
+My editorial solution is commented below
 */
 
 #include <bits/stdc++.h>
@@ -11,7 +13,7 @@ int n, a[1<<18];
 int32_t main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0);
-    cin >> n; int prev = 0;
+    cin >> n; int ans = 0;
     for(int i = 0; i < (1<<n); i++) cin >> a[i];
     for (int m=1; m<(1<<n); m++){
         int x=-1, y=-1;
@@ -23,6 +25,32 @@ int32_t main()
             else if(x<=a[s]) y=x, x=a[s];
             if(s==0) break;
         }
-        cout << max(prev, x+y) << "\n"; prev = max(prev,x+y);
+        ans = max(ans,x+y); cout << ans << "\n"; 
     }
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[1<<18];
+multiset<int> S[1<<18];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> n; int ans = 0;
+    for(int i = 0; i < (1<<n); i++) cin >> a[i], S[i].insert(a[i]);
+    
+    for(int i = 0; i < n; i++){
+        for(int m = 0; m < (1<<n); m++){
+            if((m>>i)&1) continue;
+            int x = m|(1<<i);
+            for(auto u : S[m]) S[x].insert(u);
+            while((int)S[x].size()>2) S[x].erase(S[x].begin());
+        }
+    }
+    for(int m = 1; m < (1<<n); m++)
+        ans = max(ans, *prev(S[m].end())+*prev(prev(S[m].end()))),
+        cout << ans << "\n";
+}
+*/
